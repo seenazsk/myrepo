5683e879bd44e1ced151d45bedcfd5392178e7a1
seenazsk <66836588+seenazsk@users.noreply.github.com>
2020-06-13 18:50:24 +0530

    Delete WebRole.ps1

--- WebRole.ps1
+++
@@ -1,8 +0,0 @@
-Configuration DScConfiguration {
-		Node $AllNodes.Where{$_.Role -eg "WebServer"}.NodeName
-		{
-				WindowsFeature IISInstall {
-					Ensure = 'Present'
-					Name   = 'Web-Server'
-		}
-}}
